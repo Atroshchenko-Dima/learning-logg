53b4ad9e609495cd85e9b327dd8124b24e8d8d95
Atroshchenko-Dima <dima943007@gmail.com>
2022-06-23 21:27:55 +0500

    Set DEBUG based on environment variables

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--=1$pj6j$o+d3au2v)^imy#v&ypy61q)4wsgn6_)26#!dnvtcv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
